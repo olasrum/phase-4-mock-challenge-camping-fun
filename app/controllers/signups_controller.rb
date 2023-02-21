561f1c03695733055df79d9dff5ade85f472a1e1
olasrum <mursalo@gmail.com>
2023-02-21 17:45:27 +1000

    Done with assignment

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,18 @@
+class SignupsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    def create
+        signup = Signup.create!(signup_params)
+        render json: signup.activity, status: :created  
+    end
+
+    private
+
+    def signup_params
+        params.permit(:camper_id, :activity_id, :time)
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+end
